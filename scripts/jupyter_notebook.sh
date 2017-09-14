ec85c49f26ed70a467daa238cc0d01eacda7e9ac
Gavin Noronha <gavinln@hotmail.com>
2017-09-13 19:05:45 -0700

    started Jupyter notebook bound to single ip address 192.168.33.10

--- scripts/jupyter_notebook.sh
+++ scripts/jupyter_notebook.sh
@@ -4,8 +4,9 @@
 
 ROOT_DIR=/vagrant
 export PYTHONPATH=$ROOT_DIR/python
+IP=$(hostname -I | awk  '{ print $2 }')
 
-JUPYTER_CMD="jupyter notebook --port=8888 --ip=0.0.0.0 --no-browser"
+JUPYTER_CMD="jupyter notebook --port=8888 --ip=$IP --no-browser"
 
 BASIC_NOTEBOOKS=$ROOT_DIR/notebooks
 
