023a983391335fa26c455584d1924eec842da17c
Gavin Noronha <gavinln@hotmail.com>
2020-06-12 09:32:07 -0700

    wsl automatically starts in current directory

--- win-linux.bat
+++ win-linux.bat
@@ -1,5 +1,4 @@
 @echo off
 set ROOT_DIR=%~dp0
-echo starting Windows subsystem for Linux in %ROOT_DIR%
-REM ubuntu.exe run "cd $(wslpath -u '%ROOT_DIR%'); bash"
-wsl -e /bin/bash -c "cd $(wslpath '%ROOT_DIR%');/bin/bash -l"
+REM Needed to execute direnv
+wsl -e /bin/bash -l -c "/bin/bash"
