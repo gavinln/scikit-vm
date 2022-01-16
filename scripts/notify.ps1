1d05a8069793759914cfb7326b79a945e7deeb24
Gavin Noronha <gavinln@hotmail.com>
2022-01-16 12:07:55 -0800

    add powershell notify script

---
+++ scripts/notify.ps1
@@ -0,0 +1,38 @@
+# Save this is a file notify.ps1 and execute as below
+# CMD shell: powershell.exe -File notify.ps1 my-title my-message
+# Powershell: .\notify.ps1 "my title" "my message"
+# WSL: /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -File notify.ps1 "my title" "my message"
+
+function Show-Notification {
+    [cmdletbinding()]
+    Param (
+        [string]
+        $ToastTitle,
+        [string]
+        [parameter(ValueFromPipeline)]
+        $ToastText
+    )
+
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+    $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+
+    $RawXml = [xml] $Template.GetXml()
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+
+    $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
+    $SerializedXml.LoadXml($RawXml.OuterXml)
+
+    $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
+    $Toast.Tag = "PowerShell"
+    $Toast.Group = "PowerShell"
+    $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+    $Notifier.Show($Toast);
+}
+
+$title = $args[0]
+$message =$args[1]
+
+Show-Notification "$title" "$message"
